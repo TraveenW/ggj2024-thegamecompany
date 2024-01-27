a2e9086171f6c0818a28a2236bd16a176fdc50ac
TraveenW <132418873+TraveenW@users.noreply.github.com>
2024-01-27 15:08:11 +1100

    Added Audio Manager
    Added Audio Manager script, a prefab and my own scene to test things out.

---
+++ thegamecompany/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,136 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("Volumes")]
+    [SerializeField] float musicVolume = 1;
+    [SerializeField] float sfxVolume = 1;
+
+    [Header("Music")]
+    public AudioSource backgroundMusic;
+    [SerializeField] float fadeOutTimeMusic = 4;
+    private bool isMusicPlaying = false;
+
+    [Header("Footsteps")]
+    [SerializeField] float footstepPitchRange = 0.15f;
+    public AudioSource footstepsSource;
+    [SerializeField] AudioClip[] streetFootsteps;
+    [SerializeField] AudioClip[] metalFootsteps;
+    private int footstepSurface = 0;
+
+    [Header("Meteor Sounds")]
+    public AudioSource meteorFall;
+    public AudioSource meteorCrash;
+
+    [Header("Audience Sounds")]
+    public AudioSource crowdLaugh;
+    public AudioSource crowdSad;
+    public AudioSource crowdCheer;
+
+    [Header("Misc")]
+    public AudioSource fountainStream;
+    private bool isfountainPlaying = false;
+    public AudioSource woodHit;
+
+    // Create audio manager as a singleton that lasts between scenes
+    // Access audiomanager functions in other scripts using: AudioManager.Instance.[public function/variable]
+    public static AudioManager Instance { get; private set; }
+    private void Awake()
+    {
+        // If another manager is present, delete that manager
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Start background music if it isn't playing already
+    public void PlayMusic()
+    {
+        if (backgroundMusic.isPlaying!)
+        {
+            backgroundMusic.volume = musicVolume;
+            backgroundMusic.Play();
+            isMusicPlaying = true;
+        }
+    }
+
+    // Play random footstep depending on surface type
+    public void PlayFootsteps()
+    {
+        int clipIndex;
+
+        footstepsSource.pitch = Random.Range(1 - footstepPitchRange / 2, 1 + footstepPitchRange / 2);
+        footstepsSource.volume = sfxVolume;
+
+        switch (footstepSurface)
+        {
+            case 1:
+                clipIndex = Random.Range(0, metalFootsteps.Length);
+                footstepsSource.PlayOneShot(metalFootsteps[clipIndex]);
+                break;
+            default:
+                clipIndex = Random.Range(0, streetFootsteps.Length);
+                footstepsSource.PlayOneShot(streetFootsteps[clipIndex]);
+                break;
+        }
+    }
+
+    // Switch footstepSurface to street or metal depending on which is called
+    public void SwitchFootstepsStreet() { footstepSurface = 0; }
+    public void SwitchFootstepsMetal() {  footstepSurface = 1; }
+
+
+    // Start fountain stream sounds if it isn't playing already
+    public void PlayStream()
+    {
+        if (fountainStream.isPlaying!)
+        {
+            fountainStream.volume = sfxVolume;
+            fountainStream.Play();
+            isMusicPlaying = true;
+        }
+    }
+
+    // Stops fountain stream sounds if it hasn't stopped yet
+    public void StopStream()
+    {
+        if (fountainStream.isPlaying)
+        {
+            fountainStream.Stop();
+            isMusicPlaying = false;
+        }
+    }
+
+    // Fade out music over time
+    public void StartFadeOutMusic()
+    {
+        StartCoroutine(FadeOutMusic());
+    }
+
+    private IEnumerator FadeOutMusic()
+    {
+        float fadeAmount = musicVolume * fadeOutTimeMusic * Time.deltaTime;
+
+        while (backgroundMusic.volume > 0)
+        {
+            backgroundMusic.volume -= fadeAmount;
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+    }
+
+
+}
