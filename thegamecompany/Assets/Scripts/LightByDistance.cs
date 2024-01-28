5e2a7cedc1ced0ceeac0af8e45b21777c4451022
YOYBROY <154099724+YOYBROY@users.noreply.github.com>
2024-01-28 11:22:03 +1100

    Added Post Processing and Fountain Collider

---
+++ thegamecompany/Assets/Scripts/LightByDistance.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightByDistance : MonoBehaviour
+{
+    private GameObject player;
+    private Vector3 lightPosition;
+    private Light light;
+
+    [SerializeField]
+    private float activateDistance;
+
+    void OnAwake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        lightPosition = this.transform.position;
+        light = this.GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        Vector3 playerPos = player.transform.position;
+        float distance = Vector3.Distance(playerPos, lightPosition);
+
+        if(distance <= activateDistance)
+        {
+            light.enabled = true;
+        }else light.enabled = false;
+    }
+}
